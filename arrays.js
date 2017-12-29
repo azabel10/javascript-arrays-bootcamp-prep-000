85d5664d98b37393cb38231eefe6ac1123655d49
Aza Gutierrez <azabel10@gmail.com>
2017-12-29 19:37:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+var addElementToBeginningofArray = [ “2’, “3”, “4”];
+function addElementToBeginningofArray (){
+	addElementToBeginningofArray.unshift ("1");
+return addElementToBeginningofArray
+}
+
+var destructiveAddElementToBeginningOfArray = [“11”, “12”, “13”];
+function destructiveAddElementToBeginningOfArray() {
+destructiveAddElementToBeginningOfArray.unshift ("10");
+return destructiveAddElementToBeginningOfArray
+}
